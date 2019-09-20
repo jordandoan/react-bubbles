f48f43953bc2cfc73bd77cb7ccca79ac647dbfe9
Jordan <jordandoan@hotmail.com>
2019-09-20 09:08:52 -0700

    Implemented put and delete requests for color page

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { axiosWithAuth } from '../axiosWithAuth';
 
 const initialColor = {
   color: "",
@@ -7,7 +7,6 @@ const initialColor = {
 };
 
 const ColorList = ({ colors, updateColors }) => {
-  console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
 
@@ -18,13 +17,19 @@ const ColorList = ({ colors, updateColors }) => {
 
   const saveEdit = e => {
     e.preventDefault();
-    // Make a put request to save your updated color
-    // think about where will you get the id from...
-    // where is is saved right now?
+    const URL = `http://localhost:5000/api/colors/${colorToEdit.id}`
+    axiosWithAuth().put(URL, colorToEdit)
+      .catch(err => console.log(err));
+    let newColors = colors.map(oldColor => (oldColor.id != colorToEdit.id ? oldColor : colorToEdit));
+    updateColors(newColors);
   };
 
   const deleteColor = color => {
-    // make a delete request to delete this color
+    const URL = `http://localhost:5000/api/colors/${color.id}`;
+    axiosWithAuth().delete(URL)
+      .catch(err => console.log(err));
+    let newColors = colors.filter(oldColor => oldColor.id != color.id);
+    updateColors(newColors);
   };
 
   return (
